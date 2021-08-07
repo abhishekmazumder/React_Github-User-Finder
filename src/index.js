c1f5105615eb13bfa39a9662c86b85a569a21f2c
abhishek <abhishekmazumder69@gmail.com>
2021-08-07 22:36:43 +0530

    Add Authentication using Auth0

--- src/index.js
+++ src/index.js
@@ -6,16 +6,22 @@ import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+
 ReactDOM.render(
 	<React.StrictMode>
-		<GithubProvider>
-			<App />
-		</GithubProvider>
+		<Auth0Provider
+			domain="dev-0wgb95kc.us.auth0.com"
+			clientId="lp7lQ6W91EgiTmjSvQsCH9shbGST2zWP"
+			redirectUri={window.location.origin}
+			cacheLocation="localstorage"
+		>
+			<GithubProvider>
+				<App />
+			</GithubProvider>
+		</Auth0Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -16,7 +16,7 @@ const Dashboard = () => {
 	}
 	return (
 		<main>
-			<Navbar></Navbar>
+			<Navbar />
 			<Search />
 			<Info />
 			<User />
